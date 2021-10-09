96db84e31735e81af5235727c1103dbfe0dfb180
Morgbork <nikitaivlad@yandex.ru>
2021-10-10 02:18:40 +0300

    fix

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -25,8 +25,7 @@ class RecipeListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        exclude = ['protein', 'carbohydrates', 'fats', 'calories',  'average_star',
-                   'is_favourite', 'favourite_count']
+        exclude = ['protein', 'carbohydrates', 'fats', 'calories']
 
     def get_stars_count(self, obj):
         return Star.objects.filter(content_type=ContentType.objects.get_for_model(obj), object_id=obj.id).count()
