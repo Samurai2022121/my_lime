a9c17d81485a2d25d70243aa6377d47a6bdad1d4
Morgbork <nikitaivlad@yandex.ru>
2021-10-20 17:13:01 +0300

    remove username field

--- users/models.py
+++ users/models.py
@@ -16,6 +16,7 @@ from .managers import CustomUserManager
 class User(AbstractUser):
     first_name = None
     last_name = None
+    username = None
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message='Phone number must be entered in the format: "+999999999". '
                                          'Up to 15 digits allowed.')
