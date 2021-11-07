940d19a64a3737b136d2d47ba903be250c3cb876
Morgbork <nikitaivlad@yandex.ru>
2021-11-07 05:00:25 +0300

    fix

--- products/signals.py
+++ products/signals.py
@@ -9,13 +9,9 @@ from utils.models_utils import compress_image
 
 @receiver(post_save, sender=ProductImages)
 def save_product_images(sender, instance=None, created=False, **kwargs):
-    post_save.disconnect(save_product_images, sender=sender)
-    with deepcopy(instance.image_1000) as temp:
-        instance.image_500 = compress_image(temp, (500, 500), 'image_500',  ('jpeg', 'jpg'))
-        instance.image_150 = compress_image(temp, (150, 150), 'image_150',  ('png', 'png'))
-        instance.image_1000 = compress_image(temp, (1000, 1000), 'image_1000', ('jpeg', 'jpg'))
-        instance.save()
-    post_save.connect(save_product_images, sender=sender)
-
-
-post_save.connect(save_product_images, sender=ProductImages)
+    if created:
+        with deepcopy(instance.image_1000) as temp:
+            instance.image_500 = compress_image(temp, (500, 500), 'image_500',  ('jpeg', 'jpg'))
+            instance.image_150 = compress_image(temp, (150, 150), 'image_150',  ('png', 'png'))
+            instance.image_1000 = compress_image(temp, (1000, 1000), 'image_1000', ('jpeg', 'jpg'))
+            instance.save()
