dccf67bbdb1f038c7d0e2da77295d9821e4c3cf5
vlad <vlad@vlad.com>
2021-12-10 16:38:51 +0300

    add GET permission

--- news/views.py
+++ news/views.py
@@ -1,12 +1,19 @@
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, BasePermssion
 
 from utils.views_utils import ProductPagination
 
 from .models import News, Section
 from .serializers import NewsSerializer, SectionSerializer
+
+
+class ReadOnlyPermissions(BasePermssion):
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+        return request.user and request.user.is_authenticated
 
 
 class NewsFilter(django_filters.FilterSet):
@@ -21,7 +28,7 @@ class NewsViewset(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     pagination_class = ProductPagination
     filterset_class = NewsFilter
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (ReadOnlyPermissions,)
     serializer_class = NewsSerializer
     lookup_field = "id"
     queryset = News.objects.all()
