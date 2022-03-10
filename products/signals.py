5c9ffc86fa0dc931f23192b4211cdd90b7016fa4
Morgbork <45299305+Morgbork@users.noreply.github.com>
2022-03-10 20:41:21 +0300

    adjust image formats (#30)
    Co-authored-by: vlad <vlad@vlad.com>

--- products/signals.py
+++ products/signals.py
@@ -13,12 +13,12 @@ def save_product_images(sender, instance=None, created=False, **kwargs):
     if created:
         with deepcopy(instance.image_1000) as temp:
             instance.image_500 = compress_image(
-                temp, (500, 500), "image_500", ("jpeg", "jpg")
+                temp, (500, 500), "image_500", ("png", "png")
             )
             instance.image_150 = compress_image(
                 temp, (150, 150), "image_150", ("png", "png")
             )
             instance.image_1000 = compress_image(
-                temp, (1000, 1000), "image_1000", ("jpeg", "jpg")
+                temp, (1000, 1000), "image_1000", ("png", "png")
             )
             instance.save()
